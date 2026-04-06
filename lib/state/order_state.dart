90f8754104be12b5e258750d2ceda8c975784479
SASCHADDD <saschadanu23@gmail.com>
2026-04-06 09:36:05 +0700

    membuat model untuk menyimpan

--- lib/state/order_state.dart
+++ lib/state/order_state.dart
@@ -6,3 +6,20 @@ abstract class OrderState extends Equatable {
 class OrderInitial extends OrderState {}
 
 class OrderLoading extends OrderState {}
+
+class OrderSuccess extends OrderState {
+  final String makanan;
+  final String minuman;
+  final int jumlahMakanan;
+  final int jumlahMinuman;
+  final int totalHarga;
+
+  OrderSuccess({
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+    required this.totalHarga,
+  });
+  
+
