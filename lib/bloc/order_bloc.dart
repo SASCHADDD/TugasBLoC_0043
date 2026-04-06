a33069c7dd5a2d3729b0f1583f0fe7061ea794e2
SASCHADDD <saschadanu23@gmail.com>
2026-04-06 09:30:16 +0700

    membuat model untuk menyimpan dari event

--- lib/bloc/order_bloc.dart
+++ lib/bloc/order_bloc.dart
@@ -3,3 +3,16 @@ abstract class OrderEvent extends Equatable {
   @override
   List<Object?> get props => [];
 }
+
+class OrderSubmitted extends OrderEvent {
+  final String makanan;
+  final String minuman;
+  final int jumlahMakanan;
+  final int jumlahMinuman;
+
+  OrderSubmitted({
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+  });
