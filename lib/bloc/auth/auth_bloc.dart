75f2c61c80e232ae13b7ecfaf11e66abbe374d8b
SASCHADDD <saschadanu23@gmail.com>
2026-04-06 07:18:07 +0700

    logic dari login

--- lib/bloc/auth/auth_bloc.dart
+++ lib/bloc/auth/auth_bloc.dart
@@ -4,4 +4,14 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     on<RegisterSubmitted>(_onRegisterSubmitted);
     on<LogoutRequested>(_onLogoutRequested);
   }
-}
+  Future<void> _onLoginSubmitted(...) async {
+    emit(AuthLoading());
+    await Future.delayed(const Duration(seconds: 1));
+
+    if (event.email.isNotEmpty && event.password.isNotEmpty) {
+      emit(AuthAuthenticated(email: event.email));
+    } else {
+      emit(AuthError(message: 'Email atau password salah'));
+    }
+  }
+
